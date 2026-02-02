4a682fb1919e96361c348157a530087a1a828000
Nick Bourgeois <77581387+Nick676191@users.noreply.github.com>
2026-02-02 07:45:18 -0600

    Create functions to display books on the page in a table from javascript

---
+++ script.js
@@ -0,0 +1,85 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+    // object constructor
+    if (!new.target) {
+        throw error("You need to use the `new` keyword when declaring this object constructor")
+    }
+    this.id = crypto.randomUUID();
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+};
+
+function addBookToLibrary() {
+    // take params, create a book then store it in the array
+    const title = "Harry Potter and the Sorcerer's Stone";
+    const author = "J.K. Rowling";
+    const pages = 322;
+    const read = true;
+
+    const newBook = new Book(title, author, pages, read);
+
+    myLibrary.push(newBook);
+};
+
+function displayBooks() {
+    // create table
+    const tableDiv = document.querySelector(".tbl-container");
+    const table = document.createElement("table");
+    const header = document.createElement("tr");
+
+    if (myLibrary.length > 0) {
+        for (const headerKey in myLibrary[0]) {
+            const headerItem = document.createElement("th");
+            headerItem.append(headerKey);
+            header.appendChild(headerItem);
+        };
+    } else {
+        const headOne = document.createElement("th");
+        headOne.appendChild("id")
+
+        const headTwo = document.createElement("th");
+        headTwo.appendChild("title");
+
+        const headThree = document.createElement("th");
+        headThree.appendChild("author");
+
+        const headFour = document.createElement("th");
+        headFour.appendChild("pages");
+
+        const headFive = document.createElement("th");
+        headFive.appendChild("read");
+
+        header.appendChild(headOne);
+        header.appendChild(headTwo);
+        header.appendChild(headThree);
+        header.appendChild(headFour);
+        header.appendChild(headFive);
+    };
+
+    tableDiv.appendChild(table);
+    table.appendChild(header);
+
+    for (const book of myLibrary) {
+        const row = document.createElement("tr");
+        table.appendChild(row);
+        for (const key in book) {
+            const dataCell = document.createElement("td");
+            dataCell.append(book[key]);
+            row.appendChild(dataCell);
+        };
+    };
+};
+
+addBookToLibrary();
+displayBooks();
+console.log(myLibrary);
+
+// for (let i of myLibrary) {
+//     for (let key in i) {
+//         console.log(i[key]);
+//     }
+// }
+
